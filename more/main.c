04c42b61f1668638638a801a5ac22e5507be4367
JC <joaquin_conobolillo@yahoo.es>
2019-05-31 04:04:42 +0200

    ### 20190531 * tabs to spaces once more

--- more/main.c
+++ more/main.c
@@ -21,34 +21,35 @@ int main(int argc, char** argv) {
     }
     else {
 
-        gST->ConOut->QueryMode(gST->ConOut, (UINTN)gST->ConOut->Mode->Mode, &Cols, &Rows);	// get number of text lines
+        gST->ConOut->QueryMode(gST->ConOut, (UINTN)gST->ConOut->Mode->Mode, &Cols, &Rows);  // get number of text lines
 
-        for (pBuf = NULL, i = 0; EOF != (c = fgetc(stdin)); i++) {							// read character from STDIN until EOF
-            pBuf = realloc(pBuf, i + 1);													// allocate buffer / increase buffersize
+        for (pBuf = NULL, i = 0; EOF != (c = fgetc(stdin)); i++) {                          // read character from STDIN until EOF
+            pBuf = realloc(pBuf, i + 1);                                                    // allocate buffer / increase buffersize
             if (NULL == pBuf)
                 abort();
-            pBuf[i] = (char)c;																// store character in memory buffer
+            pBuf[i] = (char)c;                                                              // store character in memory buffer
         }
 
-        pBuf[i] = '\0';																		// set string termination
+        pBuf[i] = '\0';                                                                     // set string termination
 
-        for (Line = 0, p = strtok(pBuf, "\n"); p != NULL; Line++)							// display the text screen wise
+        for (Line = 0, p = strtok(pBuf, "\n"); p != NULL; Line++)                           // display the text screen wise
         {
             if (Rows == 1 + Line) {
                 UINTN Index;
                 EFI_INPUT_KEY Key;
                 EFI_STATUS Status;
                 printf("-- More --");
-                do {																		// read real kbhit(), since STDIN is redirected
+                do {                                                                        // read real kbhit(), since STDIN is redirected
                     gST->BootServices->WaitForEvent(1, gST->ConIn->WaitForKey, &Index);
                     Status = gST->ConIn->ReadKeyStroke(gST->ConIn, &Key);
                 } while (EFI_SUCCESS != Status);
                 printf("\n");
                 Line = 0;
             }
-            printf("%s\n", p);																// print the text line
-            p = strtok(NULL, "\n");															// get the next text line
+            printf("%s\n", p);                                                              // print the text line
+            p = strtok(NULL, "\n");                                                         // get the next text line
         }
     }
     return EFI_SUCCESS;
 }
+
